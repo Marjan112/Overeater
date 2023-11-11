fae4599f5e0e24fef4106fea3c8519d9bf6ac276
Marjan112 <marjanramadanovic1234@gmail.com>
2023-11-11 12:42:45 +0100

    ¯\_(ツ)_/¯

--- src/main.cpp
+++ src/main.cpp
@@ -12,7 +12,6 @@
 #	pragma comment(lib, "sfml-audio.lib")
 #	pragma comment(lib, "sfml-system.lib")
 #	pragma comment(lib, "sfml-graphics.lib")
-#	pragma comment(lib, "Ws2_32.lib")
 
 #	include "../include/SFML_MSC_Windows/Graphics.hpp"
 #	include "../include/SFML_MSC_Windows/Audio.hpp"
